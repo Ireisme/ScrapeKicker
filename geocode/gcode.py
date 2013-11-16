60b341d4c8fb7121d269affb7b93cd9f372d4727
ohnopirates <jethatcher@gmail.com>
2013-11-16 12:02:43 -0500

    sexy fixes

--- geocode/gcode.py
+++ geocode/gcode.py
@@ -3,9 +3,11 @@ from pymongo import MongoClient
 from time import sleep
 from sys import argv
 
-script, dbip = argv
+script, ip = argv
+dbip = 'mongodb://'
+dbip += ip
 
-client = MongoClient('mongodb://',dbip)
+client = MongoClient(dbip)
 db = client.Kickstarter
 collection = db.users
 
@@ -29,4 +31,4 @@ while True:
 		collection.save(user)
 		sleep(.1) #this just prevents the stupid "too fast" error
 	except geocoders.googlev3.GTooManyQueriesError:
-		sleep(86400) #sit for 24 hours
+		sleep(3600) #sleep and hour, then check again
